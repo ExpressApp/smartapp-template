b1b621fe4ad78b7314a4447f6d9f34ea9855223b
Kiruha01 <liss-2001g@mail.ru>
2023-01-26 12:19:16 +0300

    bump: lib versions

--- app/smartapp/smartapp.py
+++ app/smartapp/smartapp.py
@@ -1,10 +1,10 @@
 """Configuration for smartapp instance."""
 from pybotx_smartapp_rpc import SmartAppRPC
-from pybotx_smartapp_smart_logger import smartapp_exception_handler
 
+from app.smartapp.middlewares.smartlogger import smart_logger_middleware
 from app.smartapp.rpc_methods import common
 
 smartapp = SmartAppRPC(
     routers=[common.rpc],
-    exception_handlers={Exception: smartapp_exception_handler},
+    middlewares=[smart_logger_middleware],
 )
