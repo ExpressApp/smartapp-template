937b7c5696ef0b5cfba432993a61309f80c9e3d2
Yegor Kutuzov <kutuzov-1313@yandex.ru>
2024-03-26 15:20:42 +0300

    chore: openapi url disabled if debug mode is false (#31)

--- app/api/routers.py
+++ app/api/routers.py
@@ -4,10 +4,12 @@ from fastapi import APIRouter
 from app.api.endpoints.botx import router as bot_router
 from app.api.endpoints.healthcheck import router as healthcheck_router
 from app.api.endpoints.swagger_rpc_execute import router as swagger_rpc_execute_router
+from app.settings import settings
 
 router = APIRouter(include_in_schema=False)
 
 router.include_router(healthcheck_router)
 router.include_router(bot_router)
 
-router.include_router(swagger_rpc_execute_router)
+if settings.DEBUG:
+    router.include_router(swagger_rpc_execute_router)
