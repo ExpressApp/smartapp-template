9241227a83b2fb0cb56af808d760c89ddcb0cc02
Alexander Maximenyuk <alex.maximenyuk@ccsteam.ru>
2024-06-11 02:09:01 +0200

    feat: add handler for synchronous smartapp requests

--- app/bot/commands/common.py
+++ app/bot/commands/common.py
@@ -1,7 +1,12 @@
 """Handlers for default bot commands and system events."""
 
-
-from pybotx import Bot, HandlerCollector, IncomingMessage, SmartAppEvent
+from pybotx import (
+    Bot,
+    HandlerCollector,
+    IncomingMessage,
+    SmartAppEvent,
+    SyncSmartAppRequestResponsePayload,
+)
 
 from app.smartapp.smartapp import smartapp
 
@@ -11,6 +16,13 @@ collector = HandlerCollector()
 @collector.smartapp_event
 async def handle_smartapp_event(event: SmartAppEvent, bot: Bot) -> None:
     await smartapp.handle_smartapp_event(event, bot)
+
+
+@collector.sync_smartapp_request
+async def handle_sync_smartapp_request(
+    event: SmartAppEvent, bot: Bot
+) -> SyncSmartAppRequestResponsePayload:
+    return await smartapp.handle_sync_smartapp_request(event, bot)
 
 
 @collector.command("/_test-redis-callback-repo", visible=False)
